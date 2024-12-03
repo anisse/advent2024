d3946b4694d6ff3de524e8e5c8abd16aae793356
Anisse Astier <anisse@astier.eu>
2024-12-03 06:45:19 +0100

    day03: merge parse and solve
    it's a parsing day, and parsing already has most of the logic.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,31 +1,25 @@
 use advent2024::*;
 use regex::Regex;
 fn main() {
-    let things = parse1(input!());
     //part 1
-    let res = part1(things);
+    let res = part1(input!());
     println!("Part 1: {}", res);
     //part 2
-    let things = parse2(input!());
-    let res = part2(things);
+    let res = part2(input!());
     println!("Part 2: {}", res);
 }
-type ParsedItem = (u32, u32);
-fn parse1(input: &str) -> Vec<ParsedItem> {
+fn part1(input: &str) -> u32 {
     let re = Regex::new(r"mul\(([0-9]+,[0-9]+\))").unwrap();
     re.captures_iter(input)
         .map(|c| c.extract())
         .map(|(_, [c])| {
-            let mut i = ints(c);
+            let mut i = ints::<u32>(c);
             (i.next().unwrap(), i.next().unwrap())
         })
-        .collect()
-    /*
-    input.match_indices("mul")
-        .map(|i| if input.chars().nth(i + 1) == '(
-        */
+        .map(|(a, b)| a * b)
+        .sum()
 }
-fn parse2(input: &str) -> Vec<ParsedItem> {
+fn part2(input: &str) -> u32 {
     let re =
         Regex::new(r"mul\((?P<mul>[0-9]+,[0-9]+)\)|(?P<do>do\(\))|(?P<dont>don't\(\))").unwrap();
     re.captures_iter(input)
@@ -45,22 +39,16 @@ fn parse2(input: &str) -> Vec<ParsedItem> {
             }
             Some((0, 0))
         })
-        .collect()
-}
-fn part1(things: Vec<ParsedItem>) -> u32 {
-    things.iter().map(|(a, b)| a * b).sum()
-}
-
-fn part2(things: Vec<ParsedItem>) -> u32 {
-    things.iter().map(|(a, b)| a * b).sum()
+        .map(|(a, b)| a * b)
+        .sum()
 }
 
 #[test]
 fn test() {
     //part 1
-    let res = part1(parse1(sample!()));
+    let res = part1(sample!());
     assert_eq!(res, 2 * 4 + 5 * 5 + 11 * 8 + 8 * 5);
     //part 2
-    let res = part2(parse2(sample!()));
+    let res = part2(sample!());
     assert_eq!(res, 48);
 }
