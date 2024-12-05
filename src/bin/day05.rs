33ab084e6f14b53d3b9697158742a4d5751da730
Anisse Astier <anisse@astier.eu>
2024-12-05 07:56:51 +0100

    day 5: cleanup

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    thread,
-};
+use std::collections::{HashMap, HashSet};
 
 use advent2024::*;
 fn main() {
@@ -36,23 +33,8 @@ fn parse(input: &str) -> (Vec<Order>, Vec<Update>) {
         .collect();
     (orders, updates)
 }
+
 fn part1(order: &[Order], updates: &[Update]) -> usize {
-    verify_order(order, updates)
-    /*
-    // Needs a bigger stack
-    let o2 = order.to_vec();
-    let u2 = updates.to_vec();
-
-    let child = thread::Builder::new()
-        .stack_size(32 * 1024 * 1024)
-        .spawn(move || verify_order(&o2, &u2))
-        .unwrap();
-    let outside = child.join().unwrap();
-    outside
-    */
-}
-
-fn verify_order(order: &[Order], updates: &[Update]) -> usize {
     let (obefore, oafter) = order_graph(order);
     updates
         .iter()
@@ -72,10 +54,6 @@ fn verify_order(order: &[Order], updates: &[Update]) -> usize {
                 }
             }
             true
-            /*
-            u.windows(2)
-                .all(|s| is_after(s[0], s[1], &o, &mut HashSet::new()))
-                */
         })
         .inspect(|u| {
             println!("is ok: {u:?}");
@@ -87,33 +65,11 @@ type OrderMap = HashMap<u8, HashSet<u8>>;
 fn order_graph(order: &[Order]) -> (OrderMap, OrderMap) {
     let mut order_after = HashMap::new();
     let mut order_before = HashMap::new();
-    //let mut full = vec![];
     order.iter().cloned().for_each(|(a, b)| {
         order_after.entry(a).or_insert(HashSet::new()).insert(b);
         order_before.entry(b).or_insert(HashSet::new()).insert(a);
     });
     (order_after, order_before)
-}
-
-fn is_after(a: u8, b: u8, o: &OrderMap, visited: &mut HashSet<u8>) -> bool {
-    //(0..(visited.len())).for_each(|_| print! {" "});
-    //println!("Checking {a} -> {b}");
-    visited.insert(a);
-    // We look for b in the graph after a
-    let ret = match o.get(&a) {
-        Some(l) => l.iter().any(|x| {
-            if *x == b {
-                return true;
-            }
-            if visited.contains(x) {
-                return false;
-            }
-            is_after(*x, b, o, visited)
-        }),
-        None => false,
-    };
-    visited.remove(&a);
-    ret
 }
 
 fn part2(order: &[Order], updates: &[Update]) -> usize {
